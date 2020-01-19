774ffa7ca5385b2867ea88746fdf890f97932940
CyrilKrutoy <kirakrutoy@gmail.com>
2020-01-19 15:14:03 +0200

    js2

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-// Rechteck
-
-let a = 2.4;
-b = 3.3;
-result = a * b;
-let rechteckResult = parseFloat(result.toFixed(1));
-console.log(rechteckResult, typeof rechteckResult)
 
 
+//   // Area
+
+
+// let a = 2.4,
+//     b = 3.3,
+//     result = a * b;
+// let areaResult = parseFloat(result.toFixed(1));
+// console.log(areaResult, typeof areaResult)
 
 
 
 
+
+//  //  name, age, jear
+
+
+// let age = 25,
+//     thisYear = 2020,
+//     name = 'Polina';
+
+// let yearOfBirth = thisYear - age;
+// console.log(`My name is ${name}. I was born in ${yearOfBirth}  and I'm ${age} years old`)
+// console.log(yearOfBirth)
+
+
+
+
+
+
+//  // Circle square
+
+// // option1________________________________________________
+
+// let radius = parseFloat("5.67cm"),
+//     result = Math.round(Math.PI * (Math.pow(radius, 2)));
+// console.log(`Circle square is ${result}`)
+
+
+// //option2_________________________________________________
+// let radius = parseFloat("5.67cm");
+// let resultRadius = Math.pow(radius, 2);
+// // console.log(resultRadius)
+// let resultPiRadius = Math.PI * resultRadius;
+// // console.log(resultPiRadius)
+// let result = Math.round(resultPiRadius);
+// console.log(result)   
