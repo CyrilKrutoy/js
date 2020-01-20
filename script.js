4d1bc4fd33188fd73e0467a814a1826d209457fc
CyrilKrutoy <kirakrutoy@gmail.com>
2020-01-20 21:13:42 +0200

    js2

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 
 
-//   // Area
+// Area
 
 
-// let a = 2.4,
-//     b = 3.3,
-//     result = a * b;
-// let areaResult = parseFloat(result.toFixed(1));
-// console.log(areaResult, typeof areaResult)
-
-
-
-
-
-//  //  name, age, jear
-
-
-// let age = 25,
-//     thisYear = 2020,
-//     name = 'Polina';
-// let yearOfBirth = thisYear - age;
-// console.log(`My name is ${name}. I was born in ${yearOfBirth}  and I'm ${age} years old`)
-// console.log(yearOfBirth)
+let a = 2.4,
+    b = 3.3,
+    aResult = a * b;
+let areaResult = parseFloat(aResult.toFixed(1));
+console.log(areaResult, typeof areaResult)
 
 
 
 
 
+//  name, age, jear
 
-//  // Circle square
+
+let age = 25,
+    thisYear = 2020,
+    name = 'Polina';
+let yearOfBirth = thisYear - age;
+console.log(`My name is ${name}. I was born in ${yearOfBirth}  and I'm ${age} years old`)
+
+
+
+
+// Circle square
 
 // option1________________________________________________
 
@@ -40,8 +37,6 @@ console.log(`Circle square is ${result}`)
 //option2_________________________________________________
 let radius2 = parseFloat("5.67cm");
 let resultRadius = Math.pow(radius, 2);
-// console.log(resultRadius)
 let resultPiRadius = Math.PI * resultRadius;
-// console.log(resultPiRadius)
 let result2 = Math.round(resultPiRadius);
 console.log(`Circle square is ${result2}`)   
