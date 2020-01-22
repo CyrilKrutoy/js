f92ae73bba0a83ca52623dbf450e7d80ec5cddcd
CyrilKrutoy <kirakrutoy@gmail.com>
2020-01-22 21:24:34 +0200

    js3

--- scriptsecond.js
+++ scriptsecond.js
@@ -1,34 +1,42 @@
-//Even & Odd
+// //Even & Odd
 
-let isUserNumber = confirm('Хотите узнать число?');
-console.log('Ask user: ', isUserNumber, "type: ", typeof (isUserNumber));
+// let isUserNumber = confirm('Хотите узнать число?');
+// console.log('Ask user: ', isUserNumber, "type: ", typeof (isUserNumber));
 
-if (isUserNumber) {
-    let number = +prompt("Введите число");
-    console.log('Inputed number: ', number, "type: ", typeof number);
-    if (isNaN(number)) {
-        alert('Это не число!');
-    } else {
-        if (!number) {
-            alert('Вы отказались');
-        } else {
-            if (number % 2 === 0) {
-                alert("Четное!");
-            } else {
-                alert("Не четное!");
-            }
-        }
-    }
-} else {
-    alert('Вы не согласились');
+// if (isUserNumber) {
+//     let number = +p  rompt("Введите число");
+//     console.log('Inputed number: ', number, "type: ", typeof number);
+//     if (isNaN(number)) {
+//         alert('Это не число!');
+//     } else {
+//         if (!number) {
+//             alert('Вы отказались');
+//         } else {
+//             if (number % 2 === 0) {
+//                 alert("Четное!");
+//             } else {
+//                 alert("Не четное!");
+//             }
+//         }
+//     }
+// } else {
+//     alert('Вы не согласились');
+// }
+
+
+
+
+// // Enter number 
+
+let enterNumber = +prompt('Введите число!')
+console.log('Enter number: ', enterNumber, "type: ", typeof (enterNumber))
+if (enterNumber < 0) {
+    result = enterNumber % 3 === 0 ? enterNumber ** 2 : enterNumber % 8;
 }
-
-
-
-
-// Enter number 
-
-// let enterNumber = +prompt('Введите число!')
-// console.log('Enter number: ', enterNumber, "type: ", typeof (enterNumber))
-
-// if (enterNumber < 0) {
+else if (enterNumber > 0) {
+    let more = Math.ceil(Math.sqrt(enterNumber));
+    result = more > 1 ? more ** 2 : more - 0.5;
+} else {
+    result = enterNumber.toString();
+}
+alert(`${result} (${typeof (result)})`);
